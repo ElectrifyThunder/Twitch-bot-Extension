7bbf3b4bda03c7a9b584ed0af657deaf5bd9453a
ElectrifyThunder <83486337+ElectrifyThunder@users.noreply.github.com>
2025-03-19 19:15:48 -0500

    Update dontlook.py

--- dontlook.py
+++ dontlook.py
@@ -1,6 +1,6 @@
 from twitchAPI.type import AuthScope,ChatEvent
 
-CLIENT_ID = "ue2ji7ah8337u8b7kmjabfxt7sik51"
-CLIENT_SECRET = "gjfbnr249iw25zpdi3npbfkeyv5a1l"
+CLIENT_ID = "YOUR CLIENT ID"
+CLIENT_SECRET = "YOUR CLIENT SECRET"
 USER_SCOPE = [AuthScope.CHAT_READ,AuthScope.CHAT_EDIT,AuthScope.CHANNEL_MANAGE_BROADCAST]
-TARGET_CHANNEL = "ElectrifyThunder_"
+TARGET_CHANNEL = "YOUR CHANNEL NAME"
